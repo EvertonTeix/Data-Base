ddb954c58b794ebe7b6e1dd9073c83f63977fc34
Everton Teixeira <evertonteixe99@alu.ufc.br>
2023-06-19 09:46:16 -0300

    parte 1 - projeto final

---
+++ Projeto - Parte 1/src/main/java/DAO/StudentManager.java
@@ -0,0 +1,121 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import DTO.*;
+import Connection.*;
+
+public class StudentManager {
+
+	public void addStudent(Student student) {
+
+		try {
+			Connection connection = Connecting.getInstance().getConnection();
+
+			String sql = "INSERT INTO student (NAME, EMAIL) VALUES (?, ?)";
+
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			statement.setString(1, student.getName());
+			statement.setString(2, student.getEmail());
+
+			statement.execute();
+			connection.close();
+			
+			System.out.print("Usuário inserido com sucesso.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public List<Student> getStudents() {
+		List<Student> student = new ArrayList<Student>();
+
+		try {
+
+			Connection connection = Connecting.getInstance().getConnection();
+			String sql = "SELECT * FROM student";
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				long id = resultSet.getInt("ID");
+				String nome = resultSet.getString("NAME");
+				String email = resultSet.getString("EMAIL");
+
+				Student st = new Student();
+				st.setId(id);
+				st.setName(nome);
+				st.setEmail(email);
+
+				student.add(st);
+			}
+
+			resultSet.close();
+			statement.close();
+			connection.close();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < student.size(); i++) {
+			Student student1 = student.get(i);
+			System.out.println(student1.toString());
+		}
+
+		return student;
+
+	}
+
+	public void updateStudent(Student student) {
+
+		try {
+
+			Connection connection = Connecting.getInstance().getConnection();
+			String sql = "UPDATE student SET name = ?, email = ? WHERE id = ?";
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			statement.setString(1, student.getName());
+			statement.setString(2, student.getEmail());
+			statement.setLong(3, student.getId());
+
+			statement.execute();
+			statement.close();
+			connection.close();
+			
+			System.out.print("Usuário editado com sucesso.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deleteStudent(Student student) {
+
+		try {
+
+			Connection connection = Connecting.getInstance().getConnection();
+			String sql = "DELETE from student WHERE id = ?";
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			statement.setLong(1, student.getId());
+			statement.execute();
+			statement.close();
+			connection.close();
+			
+			System.out.print("Usuário excluído com sucesso.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Projeto - Parte 1/src/main/java/Test/App.java
@@ -0,0 +1,89 @@
+package Test;
+
+import java.util.*;
+import DTO.*;
+import DAO.*;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		StudentManager studentManager = new StudentManager();
+		Student student = new Student();
+
+		int opcao_menu;
+
+		do {
+			System.out.print(
+					"\n_________ MENU _________\n\n0 - SAIR\n1 - ADICIONAR ESTUDANTE\n2 - LISTAR ESTUDANTES\n3 - ATUALIZAR ESTUDANTE\n4 - REMOVER ESTUDANTE\n");
+			System.out.print("\nDigite a opcao que deseja: ");
+			opcao_menu = input.nextInt();
+
+			switch (opcao_menu) {
+
+			case 1:
+
+				input.nextLine();
+
+				System.out.print("\nDigite o nome do estudante: ");
+				String nome = input.nextLine();
+
+				System.out.print("Digite o email do estudante: ");
+				String email = input.nextLine();
+
+				student.setName(nome);
+				student.setEmail(email);
+
+				studentManager.addStudent(student);
+
+				break;
+
+			case 2:
+
+				studentManager.getStudents();
+
+				break;
+
+			case 3:
+
+				studentManager.getStudents();
+
+				System.out.print("\nDigite o id do estudante que deseja atualizar: ");
+				long id = input.nextLong();
+				
+				input.nextLine();
+
+				System.out.print("\nDigite o novo nome do estudante: ");
+				String novo_nome = input.nextLine();
+
+				System.out.print("Digite o novo email do estudante: ");
+				String novo_email = input.nextLine();
+
+				student.setId(id);
+				student.setName(novo_nome);
+				student.setEmail(novo_email);
+
+				studentManager.updateStudent(student);
+
+				break;
+
+			case 4:
+
+				studentManager.getStudents();
+
+				input.nextLine();
+
+				System.out.print("\nDigite o id do estudante que deseja remover: ");
+				long idRemover = input.nextInt();
+
+				student.setId(idRemover);
+				studentManager.deleteStudent(student);
+
+				break;
+			}
+
+		} while (opcao_menu != 0);
+	}
+
+}
